4ed590251f2d2d8e54281a3a6ecb44dbfeefaede
Jiaqi Luo <30928683+Luojiaqimath@users.noreply.github.com>
2024-07-11 14:52:34 -0400

    Add files via upload

---
+++ NCART_Experiment/NCART_EXP/utils/timer.py
@@ -0,0 +1,21 @@
+import time
+import numpy as np
+
+
+class Timer:
+
+    def __init__(self):
+        self.save_times = []
+        self.start_time = 0
+
+    def start(self):
+        # self.start_time = time.process_time()
+        self.start_time = time.time()
+
+    def end(self):
+        # end_time = time.process_time()
+        end_time = time.time()
+        self.save_times.append(end_time - self.start_time)
+
+    def get_average_time(self):
+        return np.mean(self.save_times)
